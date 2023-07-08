f78e4e0dcc610cb646713cd9d426e40aab73b88b
Vegardhgr <vegard.groder@gmail.com>
2023-07-08 17:11:00 +0200

    Nav bar with Home, Calendar, and Log in

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import NavBar from './components/navBar/navigationBar';
+import Router from './router/router';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Router/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/navBar/navigationBar.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+
+function NavigationBar() {
+    return(
+        <nav className="navbar navbar-expand-md" style = {{backgroundColor: "#60ff70"}}>
+            <div className="container-fluid">
+              <a className="navbar-brand">Bøler Åpne Barnehage</a>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <Link to = "/" className = "nav-link">Hjem</Link>
+                    <Link to = "/kalender" className = "nav-link">Kalender</Link>
+                </div>
+                <div className="navbar-nav ms-auto">
+                    <Link to = "/log-in" className="nav-link float-end">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                        </svg>
+                    </Link>
+                </div>
+              </div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavigationBar;

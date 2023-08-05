03b663a749a475c5cfae0c9f97c56bcc79fd83c0
Vegardhgr <vegard.groder@gmail.com>
2023-08-05 18:48:42 +0200

    Implemented functionality for repeating activities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,35 +7,47 @@ import GetRepeatedActivities from './components/utils/getRepeatedActivities';
 function App() {
   document.body.style.backgroundColor = "#e8f3f1";
   const [activities, setActivities] = useState([]);
-
+  const [renderApp, setRenderApp] = useState(false);
+  const [hasFilledRepeatedTasks, setHasFilledRepeatedTasks] = useState(false);
 
   const fetchTasks = async () => {
     try {
       const response = await axios.get("/activityHandler");
       if (response.data !== null && activities.length===0) {
+        
         setActivities(response.data);
       } 
     } catch (error) {
       console.error("Error fetching tasks:", error);
     };
   }
+  
+
+  function fillWithRepeatedTasks() {
+    	const updatedActivities = activities.flatMap(activity => GetRepeatedActivities(activity, 30));
+      	setActivities([...activities, ...updatedActivities])
+      	setHasFilledRepeatedTasks(true);
+  }
   useEffect(() => {
-    if (activities.length === 0) {
-      fetchTasks();
-    } else {
-    GetRepeatedActivities(activities[0], 0)
+	if (activities.length === 0) {
+    	fetchTasks();
+		setRenderApp(true);
+    } else if (!hasFilledRepeatedTasks) {
+      	fillWithRepeatedTasks()
     }
-  }, []); 
+  }, [activities]); 
 
-
-  return (
-    <div>
-        <NavBar/>
+  return <>
+    
+      {!renderApp ? "" : 
         <div>
-          <Router activities={activities} setActivities={setActivities}/>
-        </div>
-    </div>
-  );
+          <NavBar/>
+          <div>
+            <Router activities={activities} setActivities={setActivities}/>
+          </div>
+      </div>
+      }
+  </>
 }
 
 export default App;
--- frontend/src/components/utils/getDayNames.jsx
+++ frontend/src/components/utils/getDayNames.jsx
@@ -1,11 +1,14 @@
 function GetDayName({language, capitalLetter}) {
-    const dayNamesEng = ["Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"];
-    const dayNamesNorCapital = ["Søndag, Mandag, Tirsdag, Onsdag, Torsdag, Fredag, Lørdag"];
-    const dayNamesNor = ["søndag, mandag, tirsdag, onsdag, torsdag, fredag, lørdag"];
+    const dayNamesEngCapital = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const dayNamesEng = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    const dayNamesNorCapital = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
+    const dayNamesNor = ["søndag", "mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag"];
 
-    if (language === "eng") {
+    if (language === "eng" && capitalLetter === false) {
         return dayNamesEng
     } else if (language === "eng" && capitalLetter === true) {
+        return dayNamesEngCapital
+    } else if (language === "nor" && capitalLetter === true) {
         return dayNamesNorCapital;
     } else {
         return dayNamesNor
